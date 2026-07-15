264ff18121dd1b9368fd6fc53e840d15a0b46e74
Neoscripter <sange0337@gmail.com>
2026-07-15 11:12:20 +0800

    Refactor:clear up the mess with a lot of unnecessary files

--- lua/snippets/preact_component.lua
+++ lua/snippets/preact_component.lua
@@ -6,35 +6,10 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 
--- Function to convert filename to PascalCase component name
-local function get_component_name()
-	local filename = vim.fn.expand("%:t:r")
+local fmt = require('luasnip.extras.fmt').fmt
 
-	-- Convert kebab-case or snake_case → PascalCase
-	local name = filename:gsub("[-_](%w)", function(c)
-		return c:upper()
-	end)
-
-	return name:sub(1, 1):upper() .. name:sub(2)
-end
-
--- Preact component scaffold snippet
-local preact_component = s({
-    trig = "newreactcomponent",
-	dscr = "Preact Functional Component scaffold",
-	wordTrig = true,
-	priority = 1000,
-}, {
-	t("import { NodeProps } from '@/types/nodeProps';"),
-	t({ "", "import { FC } from 'preact/compat';", "", "" }),
-	t("const "),
-	f(get_component_name, {}),
-	t({ ": FC<NodeProps> = ({ className }) => {", "    return (" }),
-	i(1),
-	t({ "", "    );", "};", "", "" }),
-	t("export default "),
-	f(get_component_name, {}),
-	t(";"),
+ls.add_snippets("lua", {
+    s("fn", fmt("function"))
 })
 
 -- Register for React/Preact file types
--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,5 +1,3 @@
--- Completion
-
 return {
 	"saghen/blink.cmp",
 	event = "VimEnter",
@@ -30,6 +28,25 @@ return {
 				-- },
 			},
 			opts = {},
+			config = function()
+				local ls = require("luasnip")
+
+				for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/user/snippets/*.lua", true)) do
+					loadfile(ft_path)()
+				end
+
+				vim.keymap.set({ "i", "s" }, "<C-k>", function()
+					if ls.expand_or_jumpable() then
+						ls.expand_or_jump()
+					end
+				end, { silent = true })
+
+				vim.keymap.set({ "i", "s" }, "<C-j>", function()
+					if ls.jumpable(-1) then
+						ls.jump(-1)
+					end
+				end, { silent = true })
+			end,
 		},
 		"folke/lazydev.nvim",
 	},
